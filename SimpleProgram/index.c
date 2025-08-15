9ce6f83071f5af558e22dca42f010551594e7f87
Sampa Nayak <sampanayak987@gmail.com>
2025-08-16 01:48:27 +0530

    Add files via upload

---
+++ SimpleProgram/index.c
@@ -0,0 +1,20 @@
+//#include<stdio.h>
+//int main(){
+//	printf("Hello world");
+//}
+
+//size of integer,float,character.
+#include <stdio.h>
+int main()
+{
+	int a=12;
+	float b=212.34;
+	char c='a';
+	printf("%d\n",a);
+	printf("%f\n",b);
+	printf("%c\n",c);
+	printf("%d\n",sizeof(a));
+	printf("%d\n",sizeof(b));
+	printf("%d\n",sizeof(c));
+	return 0;
+}
