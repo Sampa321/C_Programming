fe12b0ba46d502d29e4da7017ca2a8a126d18ecd
Sampa Nayak <sampanayak987@gmail.com>
2025-08-18 00:59:30 +0530

    Add files via upload
    print(A B C D E F G  . . . . . . . . . . . .. . . .. . . . . . .Z)
    Print(a b c d e f g h i  . . . . . . . . . . . .z)

---
+++ String/print_a_to_z_of_string.c
@@ -0,0 +1,24 @@
+//print(a b c d e f g h............................................z)
+#include<stdio.h>
+int main()
+{
+	char i;
+	//
+	for(i='a';i<='z';i++)
+	{
+		printf("%c ",i);
+	}
+	return 0;
+}
+
+//print(A B C D E F G..........................................Z)
+/*#include<stdio.h>
+int main()
+{
+	char i;
+	for(i='A';i<='Z';i++)
+	{
+		printf("%c\t",i);
+	}
+	return 0;
+}*/
