a62d91e03796b1e7785d6f1ea8ff80da3670a301
Sampa Nayak <sampanayak987@gmail.com>
2025-08-18 01:02:30 +0530

    Add files via upload
    print(a b c d e f g h . . .. .  . .. .. . .. .. . .. .. .. .. ..  z) Using ASCII value.
    Print(A B C D E F G H . . . . . . .. . .. . . . . . . Z) using ASCII Value.

---
+++ String/print_a_to_z_using_ascii_value_of_string.c
@@ -0,0 +1,23 @@
+//print(a b c d e f g h............................................z) Using ASCII value.
+#include<stdio.h>
+int main()
+{
+	int i;
+	for(i=97;i<=122;i++)
+	{
+		printf("%c\t",i);
+	}
+	return 0;
+}
+
+//print(A B C D E F G.......................................Z) Using ASCII value.
+/*#include<stdio.h>
+int main()
+{
+	int i;
+	for(i=65;i<=90;i++)
+	{
+		printf("%c\t",i);
+	}
+	return 0;
+}*/
