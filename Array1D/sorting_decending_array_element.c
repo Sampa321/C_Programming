dd4a0f5112e0c3dfc8d748b76d7eeff83ccdebb4
Sampa Nayak <sampanayak987@gmail.com>
2025-08-20 00:45:52 +0530

    Add files via upload
    sorting of descending order in an array element.

---
+++ Array1D/sorting_decending_array_element.c
@@ -0,0 +1,40 @@
+//sorting of decending order in an array element.
+#include<stdio.h>
+int main()
+{
+	int size,i,j,temp;
+	printf("enter an array size:");
+	scanf("%d",&size);
+	int arr[size];
+	printf("enter your array element:\n");
+	//array input from user:
+	for(i=0;i<size;i++)
+	{
+		printf("arr[%d]=",i);
+		scanf("%d",&arr[i]);
+	}
+	printf("before sorting:");
+	for(i=0;i<size;i++)
+	{
+		printf("%d\t",arr[i]);
+	}
+	//main part of sorting:
+	printf("\nafter sorting:");
+	for(i=0;i<size;i++)
+	{
+		for(j=i+1;j<size;j++)
+		{
+			if(arr[i]<arr[j])
+			{
+				temp=arr[i];
+				arr[i]=arr[j];
+				arr[j]=temp;
+			}
+		}
+	}
+	for(i=0;i<size;i++)
+	{
+		printf("%d\t",arr[i]);
+	}
+	return 0;
+}
